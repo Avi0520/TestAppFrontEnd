7039a3e08d73353b4a97f5dc252eb605ec7ec895
Avi0520 <your-email@example.com>
2025-04-04 11:54:55 +0530

    signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignupComponent } from './modules/auth/signup/signup.component';
 import { LoginComponent } from './modules/auth/login/login.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/signup', pathMatch: 'full' },
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
     {path: 'user', loadChildren: ()=> import('./modules/user/service/user/user.module').then(m=> m.UserModule)},
